92baa4fa13999da79603d195d6c55be51a4f7c01
Regis Santos <regis42santos@gmail.com>
2021-06-29 01:07:28 -0300

    Troca de ssh para https

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rg3915/orcamentos-vuejs.git main:gh-pages
+git push -f https://github.com/rg3915/orcamentos-vuejs.git main:gh-pages
 
 cd -
