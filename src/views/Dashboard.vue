e2e6661a861e7537b7dee92f3bfbfde1ba0c7e07
Regis Santos <regis42santos@gmail.com>
2021-07-05 01:53:48 -0300

    Insere gráfico donut para tipos de clientes. close #27

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -929,93 +929,93 @@
       </CCol>
     </CRow> -->
 
-    <CCard>
-      <CCardBody>
-        <CRow>
-          <CCol sm="5">
-            <h4 id="traffic" class="card-title mb-0">Contratos Fechados</h4>
-            <div class="small text-muted">Julho 2021</div>
-          </CCol>
-          <CCol sm="7" class="d-none d-md-block">
-            <CButton color="primary" class="float-right">
-              <CIcon name="cil-cloud-download"/>
-            </CButton>
-            <CButtonGroup class="float-right mr-3">
-              <CButton
-                color="outline-secondary"
-                v-for="(value, key) in ['Day', 'Month', 'Year']"
-                :key="key"
-                class="mx-0"
-                :pressed="value === selected ? true : false"
-                @click="selected = value"
-              >
-                {{value}}
-              </CButton>
-            </CButtonGroup>
-          </CCol>
-        </CRow>
-        <MainChartExample style="height:300px;margin-top:40px;"/>
-      </CCardBody>
-      <CCardFooter>
-        <CRow class="text-center">
-          <CCol md sm="12" class="mb-sm-2 mb-0">
-            <div class="text-muted">Visits</div>
-            <strong>29.703 Users (40%)</strong>
-            <CProgress
-              class="progress-xs mt-2"
-              :precision="1"
-              color="success"
-              :value="40"
-            />
-          </CCol>
-          <CCol md sm="12" class="mb-sm-2 mb-0 d-md-down-none">
-            <div class="text-muted">Unique</div>
-            <strong>24.093 Users (20%)</strong>
-            <CProgress
-              class="progress-xs mt-2"
-              :precision="1"
-              color="info"
-              :value="20"
-            />
-          </CCol>
-          <CCol md sm="12" class="mb-sm-2 mb-0">
-            <div class="text-muted">Pageviews</div>
-            <strong>78.706 Views (60%)</strong>
-            <CProgress
-              class="progress-xs mt-2"
-              :precision="1"
-              color="warning"
-              :value="60"
-            />
-          </CCol>
-          <CCol md sm="12" class="mb-sm-2 mb-0">
-            <div class="text-muted">New Users</div>
-            <strong>22.123 Users (80%)</strong>
-            <CProgress
-              class="progress-xs mt-2"
-              :precision="1"
-              color="danger"
-              :value="80"
-            />
-          </CCol>
-          <CCol md sm="12" class="mb-sm-2 mb-0 d-md-down-none">
-            <div class="text-muted">Bounce Rate</div>
-            <strong>Average Rate (40.15%)</strong>
-            <CProgress
-              class="progress-xs mt-2"
-              :precision="1"
-              :value="40"
-            />
-          </CCol>
-        </CRow>
-      </CCardFooter>
-    </CCard>
+    <CRow>
+      <CCol col="8">
+        <CCard>
+          <CCardBody>
+            <CRow>
+              <CCol sm="5">
+                <h4 id="traffic" class="card-title mb-0">Contratos Fechados</h4>
+                <div class="small text-muted">Julho 2021</div>
+              </CCol>
+              <CCol sm="7" class="d-none d-md-block">
+                <CButton color="primary" class="float-right">
+                  <CIcon name="cil-cloud-download"/>
+                </CButton>
+                <CButtonGroup class="float-right mr-3">
+                  <CButton
+                    color="outline-secondary"
+                    v-for="(value, key) in ['Day', 'Month', 'Year']"
+                    :key="key"
+                    class="mx-0"
+                    :pressed="value === selected ? true : false"
+                    @click="selected = value"
+                  >
+                    {{value}}
+                  </CButton>
+                </CButtonGroup>
+              </CCol>
+            </CRow>
+            <MainChartExample style="height:200px;margin-top:20px;"/>
+          </CCardBody>
+          <CCardFooter>
+            <CRow class="text-center">
+              <CCol md sm="12" class="mb-sm-2 mb-0">
+                <div class="text-muted">Alto padrão</div>
+                <strong>50%</strong>
+                <CProgress
+                  class="progress-xs mt-2"
+                  :precision="1"
+                  color="success"
+                  :value="50"
+                />
+              </CCol>
+              <CCol md sm="12" class="mb-sm-2 mb-0 d-md-down-none">
+                <div class="text-muted">Médio padrão</div>
+                <strong>20%</strong>
+                <CProgress
+                  class="progress-xs mt-2"
+                  :precision="1"
+                  color="info"
+                  :value="20"
+                />
+              </CCol>
+              <CCol md sm="12" class="mb-sm-2 mb-0">
+                <div class="text-muted">Baixo padrão</div>
+                <strong>30%</strong>
+                <CProgress
+                  class="progress-xs mt-2"
+                  :precision="1"
+                  color="warning"
+                  :value="30"
+                />
+              </CCol>
+            </CRow>
+          </CCardFooter>
+        </CCard>
+      </CCol>
+
+      <CCol col="4">
+        <CCard>
+          <CCardBody>
+            <CRow>
+              <CCol col="12">
+                <h4 id="traffic2" class="card-title mb-0">Tipos de clientes</h4>
+              </CCol>
+            </CRow>
+            <CChartDoughnutCustomer style="height:280px;margin-top:60px;"/>
+          </CCardBody>
+        </CCard>
+      </CCol>
+    </CRow>
+
     <!-- <WidgetsBrand/> -->
   </div>
 </template>
 
 <script>
 import MainChartExample from './charts/MainChartExample'
+import CChartDoughnutCustomer from './charts/CChartDoughnutCustomer.vue'
 import WidgetsDropdown from './widgets/WidgetsDropdown'
 import WidgetsBrand from './widgets/WidgetsBrand'
 
@@ -1023,6 +1023,7 @@ export default {
   name: 'Dashboard',
   components: {
     MainChartExample,
+    CChartDoughnutCustomer,
     WidgetsDropdown,
     WidgetsBrand
   },
---
+++ src/views/charts/CChartDoughnutCustomer.vue
@@ -0,0 +1,29 @@
+<template>
+  <CChartDoughnut
+    :datasets="defaultDatasets"
+    :labels="['Construtora', 'Arquitetura', 'Particular']"
+  />
+</template>
+
+<script>
+import { CChartDoughnut } from '@coreui/vue-chartjs'
+
+export default {
+  name: 'CChartDoughnutCustomer',
+  components: { CChartDoughnut },
+  computed: {
+    defaultDatasets () {
+      return [
+        {
+          backgroundColor: [
+            '#009bde',
+            '#44ad41',
+            '#ffb619',
+          ],
+          data: [55, 25, 20]
+        }
+      ]
+    }
+  }
+}
+</script>
